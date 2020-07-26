92e7dc4eda97819329a397d5086547ffc6a30704
Enri2077 <enry2077@gmail.com>
2020-07-26 16:15:53 +0200

    changed dataset folder for elysium

--- docker/run_docker_image.sh
+++ docker/run_docker_image.sh
@@ -10,8 +10,8 @@ if [ $# -eq 2 ]
         --name ${USER}_slam_benchmark_$1 \
         --user $(id -u):$(id -g) \
         --cpuset-cpus "$2" \
-        -v ~/ds/performance_modelling:/home/${USER}/ds/performance_modelling \
-        -v ~/ds/performance_modelling/ros_logs:/home/${USER}/.ros/log \
+        -v ~/datasets/private/performance_modelling:/home/${USER}/ds/performance_modelling \
+        -v ~/datasets/private/performance_modelling/ros_logs:/home/${USER}/.ros/log \
         ${USER}/slam_benchmark:v1
   else
     echo "usage: $0 container_id cpu_set"
